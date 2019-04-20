85f0a9f32223286df92062c57265764327614c15
Alex Steiner <alex.steiner@posteo.de>
2019-04-20 18:43:20 +0200

    extracted sequence classes

--- noodlepy/noodle.py
+++ noodlepy/noodle.py
@@ -5,6 +5,7 @@ import pygame.time
 from pygame.locals import *
 
 from timing import Timekeeper
+from sequence import DrumSequence, ChordSequence
 from constants import *
 
 #fourchords = [[0,4,7],[-1,2,7],[-3,0,4],[-3,0,5]]
@@ -36,48 +37,6 @@ print(f"ticksperbeat: {ticksperbeat}")
 print(f"tickspersecond: {tickspersecond}")
 print(f"framespersecond: {framespersecond}")
 print(f"ticksperframe: {ticksperframe}")
-
-class Sequence:
-    def __init__(self, seq):
-        self.seq = seq
-        self.on = False
-
-    def tick(self, time):
-        if time.instep == 0 and self.seq[time.step]:
-            self.do_on(time.bar, time.step);
-            self.on = True
-        elif self.on and time.instep == 12:
-            self.do_off(time.bar, time.step);
-            self.on = False
-
-class DrumSequence(Sequence):
-    def __init__(self, channel, note, seq, out):
-        Sequence.__init__(self, seq)
-        self.channel = channel
-        self.note = note
-        self.out = out
-
-    def do_on(self, bar, step):
-        self.out.note_on(self.channel, self.note, 127)
-
-    def do_off(self, bar, step):
-        self.out.note_off(self.channel, self.note)
-
-class ChordSequence(Sequence):
-    def __init__(self, channel, basenote, notes, seq, out):
-        Sequence.__init__(self, seq)
-        self.channel = channel
-        self.basenote = basenote
-        self.notes = notes
-        self.out = out
-
-    def do_on(self, bar, step):
-        for note in self.notes[bar % 4]:
-            self.out.note_on(self.channel, self.basenote + note, 127)
-
-    def do_off(self, bar, step):
-        for note in self.notes[bar % 4]:
-            self.out.note_off(self.channel, self.basenote + note)
 
 pygame.init()
 pygame.midi.init()
